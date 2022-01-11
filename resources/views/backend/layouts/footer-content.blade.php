074e36cd43b4a785ba7732fd7eef0bc7e96adb06
Md Yearul Islam <yearulislamonem@gmail.com>
2022-01-11 18:53:48 +0600

    template

---
+++ resources/views/backend/layouts/footer-content.blade.php
@@ -0,0 +1,16 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+		  <li class="nav-item">
+			<a class="nav-link" href="#">Develop By Yearul Islam</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="javascript:void(0)">FAQ</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="#">Purchase Now</a>
+		  </li>
+		</ul>
+    </div>
+	  &copy; 2022 <a href="#">School Management System</a>. All Rights Reserved.
+  </footer>
---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('backend/layouts/head')
+     
+  </head>
+
+<body class="hold-transition dark-skin sidebar-mini theme-primary fixed">
+	
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Header Navbar -->
+    @include('backend/layouts/header')
+  </header>
+  
+  <!-- Left side column. contains the logo and sidebar -->
+  @include('backend/layouts/sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+	  <div class="container-full">
+		<!-- Main content -->
+
+
+		@yield('title')
+		@yield('dashboard')
+
+
+
+
+        
+		<!-- /.content -->
+	  </div>
+  </div>
+
+  <!--Footer -->
+  @include('backend/layouts/footer-content')
+
+
+   <!--control-sidebar -->
+  @include('backend/layouts/control-sidebar')
+   <!-- /.control-sidebar -->
+  
+  <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+  
+</div>
+<!-- ./wrapper -->
+  	
+@include('backend/layouts/footer')
+
+	
+	
+</body>
+</html>
